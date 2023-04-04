3fb183814a36a606859b304c80f8f22fd94a4038
Shivam Kundan <shivamkundan@hotmail.com>
2023-04-03 22:20:30 -0500

    Update health_app.py
    removed commented out code

--- health_app.py
+++ health_app.py
@@ -23,52 +23,6 @@ KV_FILE='health_app.kv' # kivy design file
 resolutions=[(330, 550),(390, 844),(400, 667),(412,732),(1280,800)]
 Window.size = resolutions[2]
 
-# # ===================================================================== #
-
-# class tab_template(FloatLayout, MDTabsBase):
-# 	def __init__(self,**kwargs):
-# 		super(tab_template,self).__init__(**kwargs)
-# 		text = StringProperty()
-
-# class ItemConfirm(OneLineAvatarIconListItem):
-# 	divider = None
-
-# 	def set_icon(self, instance_check):
-# 		instance_check.active = True
-# 		check_list = instance_check.get_widgets(instance_check.group)
-# 		for check in check_list:
-# 			if check != instance_check:
-# 				check.active = False
-
-# class NavButtons(Widget):
-# 	pass
-
-# class ButtonSet(MDBoxLayout):
-# 	def __init__(self,**kwargs):
-# 		super(ButtonSet,self).__init__(**kwargs)
-
-# class ContentNavigationDrawer(MDBoxLayout):
-# 	pass
-
-# class Tab(FloatLayout, MDTabsBase):
-# 	'''Class implementing content for a tab.'''
-# 	text=StringProperty()
-
-# class ItemDrawer(OneLineIconListItem):
-# 	icon = StringProperty()
-# 	text_color = ListProperty((0, 0, 0, 1))
-
-# class DrawerList(ThemableBehavior, MDList):
-# 	def set_color_item(self, instance_item):
-# 		"""Called when tap on a menu item."""
-
-# 		# Set the color of the icon and text for the menu item.
-# 		for item in self.children:
-# 			if item.text_color == self.theme_cls.primary_color:
-# 				item.text_color = self.theme_cls.text_color
-# 				break
-# 		instance_item.text_color = self.theme_cls.primary_color
-
 # --------------------------------------------------------------------- #
 
 class SubPageBase(MDScreen):
