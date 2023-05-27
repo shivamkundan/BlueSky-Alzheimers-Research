ffd822c45e34d8b6944d9151371288f6db3986da
Shivam Kundan <shivamkundan@hotmail.com>
2023-05-27 01:36:18 -0500

    Update globals.py

--- globals.py
+++ globals.py
@@ -5,12 +5,9 @@ from kivy_imports import *
 from globals import *
 from object_defs import *
 from page_templates import *
-# from kivy_imports import *
 
 KEY_ESC=27
-
 KEY_Q=113
-
 KEY_H=104
 
 BTN_COLOR = 69/255, 90/255, 100/255, 0.9
@@ -20,6 +17,7 @@ RED=(1,0,0,0.5)
 GREEN=(0,1,0,0.5)
 WHITE=(1,1,1,1)
 BLACK=(0,0,0,1)
+
 # ============================ GLOBAL FNs ============================= #
 def chevron_left_global(curr,next_pg='RiskAssesmentPage'):
 	curr.parent.transition.direction="right"
